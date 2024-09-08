9352f828f1621a70cdb67938023f530d578e3b50
aniiishetty <122535414+aniiishetty@users.noreply.github.com>
2024-09-08 09:04:02 +0530

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -9,7 +9,7 @@ import sequelize from './models/index'; // Import the Sequelize instance
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000; // Use environment variable for port, default to 3000
+const port = process.env.PORT || 3005; // Use environment variable for port, default to 3000
 
 app.use(express.static(path.join("/home/ubuntu/event/frontend/build")));
 app.get("/", (req, res) => {
