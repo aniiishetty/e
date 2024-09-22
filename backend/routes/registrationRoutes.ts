7e33278aa4939584ac1628d449ee8d87d8e1e45e
aniiishetty <122535414+aniiishetty@users.noreply.github.com>
2024-09-22 08:59:16 +0530

    Update registrationRoutes.ts

--- backend/routes/registrationRoutes.ts
+++ backend/routes/registrationRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import upload from '../middleware/upload'; // Adjust the path if needed
-import { registerUser } from '../controllers/registrationController';
+import { registerUser, getAllRegistrations } from '../controllers/registrationController';
 
 const router = express.Router();
 
 // Apply the upload middleware to handle file uploads
 router.post('/register', upload.fields([{ name: 'photo', maxCount: 1 }, { name: 'researchPaper', maxCount: 1 }]), registerUser);
+router.get('/', getAllRegistrations);
 
 export default router;
